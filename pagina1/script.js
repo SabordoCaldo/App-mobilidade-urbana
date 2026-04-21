4448803fc2660a3bf1ef09f19112a1f0dd7a50fd
Sabor do caldo <sabordocaldo.passosmg@gmail.com>
2026-04-21 18:20:13 -0300

    Update script.js

--- pagina1/script.js
+++ pagina1/script.js
@@ -1,9 +1,14 @@
 function toggleSenha(){
   const input = document.getElementById("senha");
+  const icone = document.getElementById("iconeSenha");
 
   if(input.type === "password"){
     input.type = "text";
+    icone.innerHTML = "🙈";
+    icone.style.color = "red";
   } else {
     input.type = "password";
+    icone.innerHTML = "👁";
+    icone.style.color = "#999";
   }
 }
