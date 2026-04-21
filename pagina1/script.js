7c8c351e950ac50e7bc3b7902644b5a42a700e47
Sabor do caldo <sabordocaldo.passosmg@gmail.com>
2026-04-21 18:25:17 -0300

    Update script.js

--- pagina1/script.js
+++ pagina1/script.js
@@ -1,14 +1,34 @@
-function toggleSenha(){
-  const input = document.getElementById("senha");
-  const icone = document.getElementById("iconeSenha");
+/* OLHO SENHA */
+function toggleSenha(id, el){
+  const input = document.getElementById(id);
 
   if(input.type === "password"){
     input.type = "text";
-    icone.innerHTML = "🙈";
-    icone.style.color = "red";
-  } else {
+    el.classList.add("off");
+  }else{
     input.type = "password";
-    icone.innerHTML = "👁";
-    icone.style.color = "#999";
+    el.classList.remove("off");
   }
+}
+
+/* MÁSCARA TELEFONE */
+document.getElementById("telefone").addEventListener("input", function(e){
+  let v = e.target.value.replace(/\D/g,"");
+
+  if(v.length > 11) v = v.slice(0,11);
+
+  if(v.length > 6){
+    v = v.replace(/^(\d{2})(\d{5})(\d{0,4})$/, "($1) $2-$3");
+  } else if(v.length > 2){
+    v = v.replace(/^(\d{2})(\d{0,5})$/, "($1) $2");
+  } else{
+    v = v.replace(/^(\d*)$/, "($1");
+  }
+
+  e.target.value = v;
+});
+
+/* BOTÃO ENTRAR */
+function entrar(){
+  window.location.href = "../pagina2/";
 }
