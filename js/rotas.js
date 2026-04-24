4e554c6fcc142e8cd735dd561483ea14a37c1b01
Sabor do caldo <sabordocaldo.passosmg@gmail.com>
2026-04-23 21:57:55 -0300

    Update rotas.js

--- js/rotas.js
+++ js/rotas.js
@@ -1,5 +1,6 @@
 const ROTAS = {
   login: "/App-mobilidade-urbana/pagina1/index.html",
   cadastroEscolha: "/App-mobilidade-urbana/pagina2/index.html",
-  termos: "/App-mobilidade-urbana/pagina2/termos/termos.html"
+  cadastroMotorista: "/App-mobilidade-urbana/pagina4/index.html",
+  cadastroVeiculo: "/App-mobilidade-urbana/pagina5/index.html"
 };
